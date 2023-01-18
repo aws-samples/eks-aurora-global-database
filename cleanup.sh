62492c796c7b42576b766778aab4f03efd0511f7
kmsarabu <ksarabu@amazon.com>
2023-01-18 08:14:48 -0600

    Fixed cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -5,12 +5,9 @@ REGION2=us-west-2
 export AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output text)
 
 kubectl delete ingress,services,deployments,statefulsets -n retailapp --all
-
 kubectl delete ns retailapp --cascade=true
 
 eksctl delete iamserviceaccount --name aws-load-balancer-controller --cluster eksclu --namespace kube-system --region $REGION1
-
-aws iam get-roles 
 eksctl delete cluster --name eksclu -r ${REGION1} --force -w
 eksctl delete cluster --name eksclu -r ${REGION2} --force -w
 
@@ -38,14 +35,48 @@ aws ecr delete-repository  --repository-name retailapp/webapp --force --region $
 done
 
 #delete global accelerator
-#aws globalaccelerator  delete-accelerator --accelerator-arn arn:aws:globalaccelerator::${AWS_ACCOUNT_ID}:accelerator/e73dde04-0041-4aec-8a81-0ed3421b71f0
+accel=$(aws globalaccelerator list-accelerators --region $REGION2 --query 'Accelerators[?(Name == `eksgdb`)].AcceleratorArn' --output text)
+lsnrarn=$(aws globalaccelerator list-listeners --accelerator-arn  $accel --region $REGION2 --query 'Listeners[].ListenerArn' --output text)
+aws globalaccelerator  update-accelerator --accelerator-arn $accel --no-enabled --region $REGION2
+
+for grparn in `aws globalaccelerator list-endpoint-groups --listener-arn $lsnrarn --region $REGION2 --query 'EndpointGroups[].EndpointGroupArn' --output text`
+do
+aws globalaccelerator delete-endpoint-group --endpoint-group-arn $grparn --region $REGION2
+done
+
+aws globalaccelerator  delete-listener --listener-arn $lsnrarn --region $REGION2
+aws globalaccelerator  delete-accelerator --accelerator-arn $accel --region $REGION2
 
 aws cloudformation delete-stack --stack-name EKSGDB2 --region ${REGION2}
-# delete IAM roles EKSGDB1-C9Role%
-# delete InstanceProfiles EKSGDB1-C9InstanceProfile%
+
+aws lambda delete-function --function-name AuroraGDBPgbouncerUpdate
+aws events remove-targets --rule AuroraGDBPgbouncerUpdate --ids "1"
+aws events delete-rule --name AuroraGDBPgbouncerUpdate
+
+for role in `aws iam list-roles --query 'Roles[?starts_with(RoleName, `EKSGDB1-C9Role-`) == `true`].RoleName' --region $REGION1 --output text`
+do
+ aws iam delete-role --role-name $role
+done
+for role in `aws iam list-roles --query 'Roles[?starts_with(RoleName, `auroragdbeks-ACKGrantsRole`) == `true`].RoleName' --region $REGION1 --output text`
+do
+ aws iam delete-role --role-name $role
+done
+for role in `aws iam list-roles --query 'Roles[?starts_with(RoleName, `auroragdbeks-EKSIAMRole`) == `true`].RoleName' --region $REGION1 --output text`
+do
+ aws iam delete-role --role-name $role
+done
+for role in `aws iam list-roles --query 'Roles[?starts_with(RoleName, `auroragdbeks-WorkerNodesRole`) == `true`].RoleName' --region $REGION1 --output text`
+do
+ aws iam delete-role --role-name $role
+done
 
 aws iam delete-policy --policy-arn arn:aws:iam::${AWS_ACCOUNT_ID}:policy/AWSLoadBalancerControllerAdditionalIAMPolicy
 aws iam delete-policy --policy-arn arn:aws:iam::${AWS_ACCOUNT_ID}:policy/AWSLoadBalancerControllerIAMPolicy
+aws iam detach-role-policy --role-name aurorapgbouncerlambda --policy-arn arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole
+aws iam delete-role-policy --role-name aurorapgbouncerlambda --policy-name lambda_rds_policy
+aws iam delete-role --role-name aurorapgbouncerlambda
 
+aws cloudformation delete-stack --stack-name auroragdbeks --region ${REGION2}
+aws cloudformation delete-stack --stack-name auroragdbeks --region ${REGION1}
 aws cloudformation delete-stack --stack-name EKSGDB1 --region ${REGION2}
 aws cloudformation delete-stack --stack-name EKSGDB1 --region ${REGION1}
